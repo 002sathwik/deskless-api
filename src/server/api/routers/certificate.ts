b6fccfb5ccc54a949456e3b850ef52ba52e29f4e
Sathwik <hsathwik002@gmail.com>
2024-09-04 22:40:26 +0530

    fix:certificate_api

---
+++ src/server/api/routers/certificate.ts
@@ -0,0 +1,51 @@
+import { addCertificateZ, deleteCertificateZ, updateCertificateZ } from "~/zod/certificateZ";
+import { createTRPCRouter, protectedProcedure } from "../trpc";
+import { TRPCError } from "@trpc/server";
+
+
+
+const certificateRouter = createTRPCRouter({
+
+    addCertificate: protectedProcedure.input(addCertificateZ).mutation(async ({ ctx, input }) => {
+        const name = input.name
+
+        const nameExists = await ctx.db.certificate.findFirst({
+            where: {
+                name
+            }
+        })
+        if (nameExists) {
+            throw new TRPCError({
+                code: "BAD_REQUEST",
+                message: "Certificate Already Exists"
+            })
+        } else {
+            await ctx.db.certificate.create({
+                data: {
+                    ...input
+                }
+            })
+        }
+    }),
+
+    deleteCertificate: protectedProcedure.input(deleteCertificateZ).mutation(async ({ ctx, input }) => {
+        await ctx.db.certificate.delete({
+            where: {
+                id: input.id
+            }
+        })
+    }),
+
+    updateCertificate: protectedProcedure.input(updateCertificateZ).mutation(async ({ ctx, input }) => {
+        await ctx.db.certificate.update({
+            where: {
+                id: input.id
+            },
+            data: {
+                ...input
+            }
+        })
+    })
+})
+
+export default certificateRouter
